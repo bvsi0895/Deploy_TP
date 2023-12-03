e8a3a864340f8d541080cea2b1bb302a4f0921b7
bvsi0895 <bvsi895@gmail.com>
2023-12-03 09:41:16 -0300

    correccion dbase

--- src/App.jsx
+++ src/App.jsx
@@ -14,89 +14,79 @@ function App() {
   const [precioMax, setPrecioMax] = useState(0);
   const [filtroActual, setFiltroActual] = useState({ categoria: '', precio: 100000, ordenamiento: 'precio_desc' });
 
-  // Obtener los libros al cargar el componente
-   // useEffect(() => {
-    // fetch('http://localhost:3000') // Modo desarrollo
-    // ...
+  // Modificar aplicarFiltro para incluir el ordenamiento
+  const aplicarFiltro = () => {
+    let queryParams = '';
+    if (filtroActual.categoria) {
+      queryParams += `categoria=${filtroActual.categoria}&`;
+    }
+    queryParams += `precio=${filtroActual.precio}&`;
+    queryParams += `ordenamiento=${filtroActual.ordenamiento}`;
 
-    useEffect(() => {
-      fetch(`${import.meta.env.VITE_DB_DEPLOY}/?${queryParams}`) // Modo producción
-
-        .then((response) => response.json())
-        .then((data) => {
-          setLibros(data);
-          setLibrosFiltrados(data);
-          const maxPrecioEncontrado = data.reduce((max, libro) => Math.max(max, libro.precio_$), 0);
-          const precioMaxConIncremento = maxPrecioEncontrado * 1.10; // Incrementa en un 10%
-          const precioMaxRedondeado = Math.ceil(precioMaxConIncremento / 1000) * 1000; // Redondea hacia arriba al múltiplo de 1000 más cercano
-          setPrecioMax(precioMaxRedondeado);
-        });
-    }, []);
-
-    // Modificar aplicarFiltro para incluir el ordenamiento
-    const aplicarFiltro = () => {
-      let queryParams = '';
-      if (filtroActual.categoria) {
-        queryParams += `categoria=${filtroActual.categoria}&`;
-      }
-      queryParams += `precio=${filtroActual.precio}&`;
-      queryParams += `ordenamiento=${filtroActual.ordenamiento}`; // Agregar ordenamiento al query
-
-      // fetch(`http://localhost:3000/?${queryParams}`) // Modo desarrollo
-      fetch(`${import.meta.env.VITE_DB_DEPLOY}/?${queryParams}`) // Modo producción
-
-        .then((response) => response.json())
-        .then((data) => setLibrosFiltrados(data))
-        .catch((error) => console.error('Error:', error));
-    };
-
-    const onPriceChange = (precio) => {
-      setFiltroActual((prevState) => ({ ...prevState, precio: precio }));
-      aplicarFiltro();
-    };
-
-    // Manejar el cambio de filtro y ordenamiento
-    const handleFilterChange = (categoria) => {
-      setFiltroActual((prevState) => ({ ...prevState, categoria: categoria }));
-      aplicarFiltro();
-    };
-
-    // Agregar una función para manejar el cambio en el ordenamiento
-    const onSortChange = (ordenamiento) => {
-      setFiltroActual((prevState) => ({ ...prevState, ordenamiento: ordenamiento }));
-      aplicarFiltro();
-    };
-
-    useEffect(() => {
-      aplicarFiltro(); // Aplica el filtro cada vez que cambia filtroActual
-    }, [filtroActual]);
-
-    return (
-      <div>
-        <Navbar />
-        <SearchBar />
-        <Filtros
-          onFilterChange={handleFilterChange}
-          onPriceChange={onPriceChange}
-          onSortChange={onSortChange} // Pasar onSortChange a Filtros
-          precioMax={precioMax}
-        />
-
-        <Routes>
-          <Route
-            path={'/'}
-            element={
-              librosFiltrados.length > 0 ? (
-                <ListadoDeProductos libros={librosFiltrados} />
-              ) : (
-                <MensajeSinLibros />
-              )
-            }
-          />
-          <Route path={'/detail/:id'} element={<Detail />} />
-        </Routes>
-      </div>
-    );
+    fetch(`${import.meta.env.VITE_DB_DEPLOY}/?${queryParams}`) // Modo producción
+      .then((response) => response.json())
+      .then((data) => setLibrosFiltrados(data))
+      .catch((error) => console.error('Error:', error));
   };
 
-  export default App;
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_DB_DEPLOY}`) // Modo producción
+      .then((response) => response.json())
+      .then((data) => {
+        setLibros(data);
+        setLibrosFiltrados(data);
+        const maxPrecioEncontrado = data.reduce((max, libro) => Math.max(max, libro.precio_$), 0);
+        const precioMaxConIncremento = maxPrecioEncontrado * 1.10;
+        const precioMaxRedondeado = Math.ceil(precioMaxConIncremento / 1000) * 1000;
+        setPrecioMax(precioMaxRedondeado);
+      });
+  }, []);
+
+  const onPriceChange = (precio) => {
+    setFiltroActual((prevState) => ({ ...prevState, precio: precio }));
+    aplicarFiltro();
+  };
+
+  const handleFilterChange = (categoria) => {
+    setFiltroActual((prevState) => ({ ...prevState, categoria: categoria }));
+    aplicarFiltro();
+  };
+
+  const onSortChange = (ordenamiento) => {
+    setFiltroActual((prevState) => ({ ...prevState, ordenamiento: ordenamiento }));
+    aplicarFiltro();
+  };
+
+  useEffect(() => {
+    aplicarFiltro();
+  }, [filtroActual]);
+
+  return (
+    <div>
+      <Navbar />
+      <SearchBar />
+      <Filtros
+        onFilterChange={handleFilterChange}
+        onPriceChange={onPriceChange}
+        onSortChange={onSortChange}
+        precioMax={precioMax}
+      />
+
+      <Routes>
+        <Route
+          path={'/'}
+          element={
+            librosFiltrados.length > 0 ? (
+              <ListadoDeProductos libros={librosFiltrados} />
+            ) : (
+              <MensajeSinLibros />
+            )
+          }
+        />
+        <Route path={'/detail/:id'} element={<Detail />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
